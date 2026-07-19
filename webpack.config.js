499c9eeae1dc3634fd5931b39b8b933442fd39f7
Caplouge <allen.cccccnm@gmail.com>
2026-07-19 15:52:08 +0000

    fix: inject env vars via DefinePlugin, set main entry, update README links

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   context: path.resolve(__dirname, "./"),
   target: "webworker",
   mode: "production",
+  plugins: [
+    new webpack.DefinePlugin({
+      CUSTOM_DOMAIN: JSON.stringify(process.env.CUSTOM_DOMAIN || "libcuda.so"),
+      MODE: JSON.stringify(process.env.MODE || "production"),
+      TARGET_UPSTREAM: JSON.stringify(process.env.TARGET_UPSTREAM || ""),
+    }),
+  ],
   optimization: {
     usedExports: true,
   },
